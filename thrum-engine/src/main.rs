c88b6819c8081212b4c507ad975a244e7141dd5c
RedCatstone <114140180+RedCatstone@users.noreply.github.com>
2026-09-06 16:34:36 +0200

    ADD very basic REPL (use with `cargo run` without any args)

--- thrum-engine/src/main.rs
+++ thrum-engine/src/main.rs
@@ -1,16 +1,21 @@
-use std::{env, fs};
+use std::{env, fs, io::{self, Write}};
 use thrum_engine::run_code;
 
 
 
-fn main() -> Result<(), ()> {
+fn main() {
     // unsafe { std::env::set_var("RUST_BACKTRACE", "1") }
 
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        eprintln!("Usage: thrum <file>");
-        return Err(());
+        loop {
+            print!("\n> ");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            _ = run_code(&input);
+        }
     }
 
     let file_path = &args[1];
@@ -18,6 +23,4 @@ fn main() -> Result<(), ()> {
 
     println!("\n\n\n--- START ({file_path}) ---");
     _ = run_code(&file_source_code);
-
-    Ok(())
 }
